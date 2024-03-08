0fe3c7b72bff02b07acce08edc5179ed41be50a1
AlteredMinds <116692103+AlteredMinds@users.noreply.github.com>
2024-03-07 18:50:01 -0600

    minor changes
    minor fixes and expanded about section

--- OTCNetConfig.ps1
+++ OTCNetConfig.ps1
@@ -1,8 +1,9 @@
 ﻿<#
+Script Name: OTC Network Configuration Utility
 Author: Christopher Bates
-Date: 2-29-2024
-Description: Small utility designed for automating tasks and network mapping on a OTC workstation.
-Version: 1.55
+Date: 2024-03-07
+Description: This script provides a menu-driven interface for configuring network settings and automating tasks on an OTC workstation.
+Version: 1.58
 Contact: cb0988836@otc.edu
 #>
 
@@ -23,7 +24,7 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 ########################### Functions ###########################
 
 
-#Echo Request Setup function#
+###Echo Request Setup function###
 function EchoSetup()
 {
     #Ask user whether to disable or enable echo requests#
@@ -62,7 +63,7 @@ function EchoSetup()
 }
 
 
-#Ip Setup function#
+###Ip Setup function###
 function IpSetup()
 {
     Write-Host ""
@@ -80,7 +81,7 @@ function IpSetup()
     #Format default gateway with class number#
     $gateway = "192.168." + $class + ".254"
 
-    #Get all adapters#
+    #Get network adapters#
     Get-NetAdapter | Select-Object ifIndex, Name, InterfaceDescription | Sort-Object -Property ifIndex | Format-Table
 
     #Ask user for adapter choice#
@@ -103,9 +104,19 @@ function IpSetup()
     $confirmation = Read-Host -Prompt " > Do you want to apply these settings? [Y/n]"
 
     #If input is invalid, skip network setup process#
-    if ($class -notin 0..255 -or $computer -notin 1..255)
+    if ($class -notin 0..255 -and $computer -notin 1..255)
     {
-        Write-Host " Invalid class or computer number..." -ForegroundColor Yellow
+        Write-Host " Invalid everything..." -ForegroundColor Yellow
+        Write-Host ""
+    }
+    elseif ($computer -notin 1..255)
+    {
+        Write-Host " Invalid computer number..." -ForegroundColor Yellow
+        Write-Host ""
+    }
+    elseif ($class -notin 0..255)
+    {
+        Write-Host " Invalid classroom number..." -ForegroundColor Yellow
         Write-Host ""
     }
     else 
@@ -114,8 +125,9 @@ function IpSetup()
         {
             #Prepare adapter for new settings#
             Remove-NetIPAddress -InterfaceIndex $adapter.ifIndex -AddressFamily IPv4 -Confirm:$false
-                Remove-NetRoute -InterfaceIndex $adapter.ifIndex -Confirm:$false -ErrorAction SilentlyContinue
+            Remove-NetRoute -InterfaceIndex $adapter.ifIndex -Confirm:$false -ErrorAction SilentlyContinue
             Set-NetIPInterface -InterfaceIndex $adapter.ifIndex -Dhcp Disabled
+
             #Apply new settings#
             New-NetIPAddress -InterfaceIndex $adapter.ifIndex -IPAddress $ip -PrefixLength 24 -DefaultGateway $gateway
             Set-DnsClientServerAddress -InterfaceIndex $adapter.ifIndex -ServerAddresses ("172.30.0.42","172.30.0.43")
@@ -130,7 +142,7 @@ function IpSetup()
 }
 
 
-#Net Settings function#
+###Net Settings function###
 function NetSettings()
 {
     #Check if echo requests are enabled#
@@ -151,30 +163,28 @@ function NetSettings()
     #Display packet information#
     $adapterStatistics = Get-NetAdapterStatistics
     $adapterStatistics | Select-Object Name, 
-						     @{Name='In'; Expression={'-->'}},
 						     @{Name='Uni->'; Expression={$_.ReceivedUnicastPackets}},
 						     @{Name='Multi->'; Expression={$_.ReceivedMulticastPackets}},
 						     @{Name='Broad->'; Expression={$_.ReceivedBroadcastPackets}},
-						     @{Name='Out'; Expression={'<--'}},
 						     @{Name='Uni<-'; Expression={$_.SentUnicastPackets}},
 						     @{Name='Multi<-'; Expression={$_.SentMulticastPackets}},
-						     @{Name='Broad<-'; Expression={$_.SentBroadcastPackets}} | Format-Table -AutoSize -Property Name, Uni<-, Multi<-, Broad<-, Out,
-						     @{Label="Packets"; Expression={"   |  "}}, In, Uni->, Multi->, Broad->
+						     @{Name='Broad<-'; Expression={$_.SentBroadcastPackets}} | Format-Table -AutoSize -Property Name, Uni<-, Multi<-, Broad<-,
+						     @{Label="Out Packets In"; Expression={"  <--  |  -->"}}, Uni->, Multi->, Broad->
 
     #Display connected network adapters#
-    Get-NetIPConfiguration | ?{$_.NetAdapter.Status -ne "Disconnected"} | fl
+    Get-NetIPConfiguration | ?{$_.NetAdapter.Status -ne "Disconnected"} -ErrorAction SilentlyContinue | fl
 
     #Create an object containing running processes and properties#
     $obj = @()
-    Foreach($process In (Get-Process -IncludeUserName | where {$_.UserName} | `
-      select Id, ProcessName, UserName)) {
+    Foreach($process In (Get-Process -IncludeUserName | where {$_.UserName} | select Id, ProcessName, UserName)) 
+    {
 	      $properties = @{ 'PID'=$process.Id;
 					       'ProcessName'=$process.ProcessName;
 					       'UserName'=$process.UserName;
 					     }
 	      $processProperties = New-Object -TypeName psobject -Property $properties
 	      $obj += $processProperties
-      }
+    }
 
     Write-Host "********************" -ForegroundColor DarkGray -NoNewline; Write-Host "Active Network Connections" -ForegroundColor Cyan -NoNewline; Write-Host "********************" -ForegroundColor DarkGray
 
@@ -191,7 +201,7 @@ function NetSettings()
 }
 
 
-#NetScan function#
+###NetScan function###
 function NetScan()
 {
     $j = 0
@@ -238,11 +248,12 @@ function NetScan()
 	    #Reset progress bar#
 	    Write-Progress -Activity "Search in Progress" -Completed $true
     }
+    Write-Host ""
     Read-Host -Prompt "$foundHosts hosts found. Press RETURN to go back to Menu"
 }
 
 
-#Map Studata function#
+###Map Studata function - Work in Progress###
 function MapStudata()
 {
     #Create an array of already used drive letters#
@@ -251,7 +262,7 @@ function MapStudata()
     #Ask user for info#
     Write-Host ""
     $username = Read-Host -Prompt " > OTC Username"
-    $password = Read-Host -Prompt " > Password" -AsSecureString
+    $password = Read-Host -Prompt " > Password"
     $studataLetter = Read-Host -Prompt " > Drive Letter"
     while ($driveletters -contains $studataLetter)
     {
@@ -260,15 +271,82 @@ function MapStudata()
     }
 
     #Assign drive letter to the studata drive and save credentials#
-    $FQ_Letter = $studataLetter + ':'
-    $FQ_username = "otc\" + $username
-    New-SmbMapping -LocalPath $FQ_Letter -RemotePath \\fs-studata.otc.edu\studata\iti -user $FQ_username -password $password -Persistent $true
-
+    $FQ_Command = '/c net use ' + $studataLetter + ': \\fs-studata.otc.edu\studata\iti /user:otc\' + $username + ' ' + $password + ' /persistent:Yes'
+    Start-Process -FilePath "cmd.exe"  -ArgumentList $FQ_Command -Wait
+    Write-Host ""
+    Get-SmbConnection
+    Write-Host ""
     Read-Host -Prompt "Press RETURN to go back to Menu"
 }
 
 
-#Fake Backdoor function#
+###About Information function###
+function About()
+{
+	#Display about information#
+Write-Host "___|_1_|___|___|___|___|___|___|___|___|___|___" -BackgroundColor DarkRed -ForegroundColor Gray
+Write-Host "_|___|___|___|___|" -BackgroundColor DarkRed -ForegroundColor Gray -NoNewline
+Write-Host "OTC Network" -BackgroundColor DarkRed -ForegroundColor Yellow -NoNewline
+Write-Host "|___|___|___|___|_" -BackgroundColor DarkRed -ForegroundColor Gray
+Write-Host "___|___|___|_" -BackgroundColor DarkRed -ForegroundColor Gray -NoNewline
+Write-Host "Configuration Utility" -BackgroundColor DarkRed -ForegroundColor Yellow -NoNewline
+Write-Host "_|___|_2_|___" -BackgroundColor DarkRed -ForegroundColor Gray
+Write-Host "_|___|___|___|___|___|___|___|___|___|___|___|_" -BackgroundColor DarkRed -ForegroundColor Gray
+Write-Host "___|___|___|___|___|___|___|___|___|___|___|___" -BackgroundColor DarkRed -ForegroundColor Gray
+Write-Host "_|___|_3_|___|___|___|___|___|__" -BackgroundColor DarkRed -ForegroundColor Gray -NoNewline
+Write-Host "Created by" -BackgroundColor DarkRed -ForegroundColor Yellow -NoNewline
+Write-Host "___|_" -BackgroundColor DarkRed -ForegroundColor Gray
+Write-Host "___|___|___|___|___|_4_|___|___|___" -BackgroundColor DarkRed -ForegroundColor Gray -NoNewline
+Write-Host "Chris Bates" -BackgroundColor DarkRed -ForegroundColor Yellow -NoNewline
+Write-Host "_" -BackgroundColor DarkRed -ForegroundColor Gray
+Write-Host "_|___|___|___|___|___|___|_5_|___|___|___|___|_" -BackgroundColor DarkRed -ForegroundColor Gray
+
+	#Display other projects#
+    Write-Host ""
+	Write-Host "Another Brick in the Wall:" -ForegroundColor Yellow
+	Write-Host " 1) Calculator" -ForegroundColor Gray
+	Write-Host " 2) Eco Server Utility" -ForegroundColor Gray
+	Write-Host " 3) HahaHash Algorithm " -ForegroundColor Gray
+	Write-Host " 4) Eco Mods" -ForegroundColor Gray
+    Write-Host " 5) Unity-powered Game" -ForegroundColor Gray
+	Write-Host " 6) Back to Menu" -ForegroundColor Gray
+    Write-Host ""
+
+	#Ask user to select option and display website or exit if 5#
+	$siteSelect = 0
+	while ($siteSelect -notin 1..6)
+	{
+		$siteSelect = Read-Host -Prompt " > Select an option"
+		switch ($siteSelect)
+		{
+			'1'
+			{
+				Start-Process "https://github.com/AlteredMinds/Calculator"
+			}
+			'2'
+			{
+				Start-Process "https://github.com/AlteredMinds/EcoServerUtility"
+			}
+			'3'
+			{
+				Start-Process "http://www.planet-express.delivery/hahahash.htm"
+			}
+			'4'
+			{
+				Start-Process "https://mod.io/g/eco/u/alteredminds"
+			}
+            '5'
+			{
+				Write-Host "Disclaimer: This game contains depictions of illegal activities and themes some players may find offensive and is intended for entertainment purposes only. Contact author for inquiries." -ForegroundColor Yellow
+                Write-Host ""
+                Read-Host -Prompt "Press RETURN to go back to Menu"
+			}
+		}
+	}
+}
+
+
+###Fake Backdoor function###
 function FakeBackdoor()
 {
     #Display a fake installation process#
@@ -339,16 +417,17 @@ function FakeBackdoor()
 ############################# Main #############################
 
 #Variables#
+$easterEgg = $false
 $invalid = " Please choose a valid option..."
 $scriptName = $MyInvocation.MyCommand.Name
 $installed = Test-Path 'C:\Program Files\WindowsPowerShell\Scripts\OTCNetConfig.ps1'
-$version = 'version 1.55'
+$version = 'version 1.58'
 $colors = @('DarkGreen', 'Cyan', 'Magenta', 'Yellow', 'Blue', 'DarkRed')
 $randomColor = "Cyan"
 $ruleName = (Get-NetFirewallRule | Where-Object {$_.DisplayName -like '*Echo Request - ICMPv4-In*' -and $_.Profile -like '*Private*'}).Name
 $exit = 0
 
-##Begin##
+##Continue to display menu unless exit equals 1##
 for ($i = 1; ($i * $exit) -lt 1; $i++)
 {
     Clear-Host
@@ -374,64 +453,56 @@ for ($i = 1; ($i * $exit) -lt 1; $i++)
     Write-Host " 2) Display Network Info" -ForegroundColor Gray
     Write-Host " 3) Ip Configuration Wizard" -ForegroundColor Gray
     Write-Host " 4) Network Host Scan" -ForegroundColor Gray
-    Write-Host " 5) Studata Setup" -ForegroundColor Gray
-    Write-Host " 6) About" -ForegroundColor Gray
-    Write-Host " 7) Exit" -ForegroundColor Gray
-    #Write-Host " ;) Install Backdoor" -ForegroundColor Gray #Easter Egg#
+    Write-Host " 5) About" -ForegroundColor Gray
+    Write-Host " 6) Exit" -ForegroundColor Gray
+    if ($easterEgg) {Write-Host " :) Install Backdoor" -ForegroundColor Gray}
     Write-Host " *************************" -ForegroundColor DarkGray
     $selection = Read-Host -Prompt " > Select an option"
 
-
+    switch ($selection)
+    {
 	  ###If selection is 1 set value of echo requests###
-		if($selection -eq "1")
-		{
+		'1'
+	    {
 			EchoSetup
 		}
 
 	  ###If selection is 2 show adapter settings###
-		elseif($selection -eq "2")
-		{
+		'2'
+	    {
 			NetSettings
 		}
 
-	  ###If selection is 3 enter ip setup###
-		elseif($selection -eq "3")
-		{
+	  ###If selection is 3 enter Ip config wizard###
+		'3'
+	    {
 			IpSetup
 		}
 
-	  ###If selection is 4 then scan for other local devices###
-		elseif($selection -eq "4")
-		{
+	  ###If selection is 4 then scan for other local hosts###
+		'4'
+	    {
 			NetScan
 		}
 
-      ###If selection is 5 then map a new drive to the studata share###
-		elseif($selection -eq "5")
-		{
-			MapStudata
+	  ###If selection is 5 then display about information###
+		'5'
+	    {
+            About
 		}
 
-	  ###Install Backdoor!!!!1!11101101010L0LoLOL###
-		elseif($selection -eq ";")
-		{
-			FakeBackdoor
-		}
-
-	  ###If selection is 7 then go to website###
-		elseif($selection -eq "6")
-		{
-			#Open browser and display website#
-			Start-Process "http://www.planet-express.delivery"
-		}
-
-	  ###If selection is 8 then exit###
-		elseif($selection -eq "7")
-		{
+	  ###If selection is 6 then exit###
+		'6'
+	    {
 			#Signal program to exit#
 			$exit = 1
 		}
-
+      ###Install Backdoor!!!!1!11101101010L0LoLOL###
+		':'
+	    {
+			if ($easterEgg) {FakeBackdoor}
+		}
+    }
 }
 #Thank you, Come agian#
 exit
